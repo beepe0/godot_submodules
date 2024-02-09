76d82d536f176d6d011bf2876d38f97562d411e2
beepe0 <dshakhun@gmail.com>
2024-02-09 03:57:00 +0100

    Added completed console_manager submodule

---
+++ attributes/CommandAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace BP
+{
+    namespace GameConsole
+    {
+        namespace Attribute
+        {
+            using System;
+
+            [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+            public class CommandAttribute : Attribute
+            {
+                public string KeyWord { get; set; }
+
+                public CommandAttribute(string keyWord)
+                {
+                    KeyWord = keyWord;
+                }
+            }
+        }
+    }
+}
---
+++ behaviours/Command.cs
@@ -0,0 +1,20 @@
+﻿namespace BP
+{
+    namespace GameConsole
+    {
+        namespace Command
+        {
+            using Godot;
+            public partial class Command : Node
+            {
+                public string CommandName { get; private set; }
+                public virtual void Initialize(string name)
+                {
+                    CommandName = name;
+                    CommandManager.Instance.AddChild(this);
+                }
+                public virtual void Execute(string[] keys) { }
+            }
+        }
+    }
+}
