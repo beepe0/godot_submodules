699ff8545fbf6dc1e44b9a1f864364b88b25f2e3
beepe0 <dshakhun@gmail.com>
2024-03-08 12:46:41 +0100

    Updated this submodule

---
+++ ComponentObject.cs
@@ -0,0 +1,23 @@
+using BP.GameConsole;
+using Godot;
+using System;
+
+namespace BP
+{
+    namespace ComponentSystem
+    {
+        public partial class ComponentObject : Node
+        {
+            public ComponentSystem Parent { get; set; }
+
+            public override void _Ready()
+            {
+                Initialize();
+            }
+            private void Initialize()
+            {
+                Parent = GetParent() as ComponentSystem;
+            }
+        }
+    }
+}
--- ComponentSystem.cs
+++ ComponentSystem.cs
@@ -1,21 +1,68 @@
 using Godot;
+using Godot.Collections;
+using System;
 
 namespace BP
 {
     namespace ComponentSystem
     {
-        public static class NodeExtensions
+        public partial class ComponentSystem : Node
         {
-            public static T GetComponent<T>(this Node node) where T : Node, new()
+            public static Dictionary<string, ComponentSystem> componentsSystems = new();
+            public Dictionary<string, ComponentObject> componentsObjects = new();
+
+            [Export] public string Tag = string.Empty;
+
+            public override void _Ready()
             {
-                return node.GetNodeOrNull<T>($"Components/{typeof(T).Name}");
+                Initialize();
             }
-            public static T AddComponent<T>(this Node node) where T : Node, new()
+            private void Initialize()
             {
+                Array<Node> children = GetChildren();
+                if (children.Count > 0)
+                {
+                    foreach (Node node in children)
+                    {
+                        if (node is ComponentObject)
+                        {
+                            GameConsole.GameConsole.Instance.DebugWarning(node.Name);
+
+                            componentsObjects.Add(node.Name, node as ComponentObject);
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(Tag))
+                {
+                    componentsSystems.Add(Tag, this);
+                }
+            }
+            public T GetComponent<T>() where T : Node, new()
+            {
+                if (!componentsObjects.ContainsKey(typeof(T).Name)) return null;
+                GameConsole.GameConsole.Instance.DebugError($"{typeof(T).Name} :: {componentsObjects[typeof(T).Name].Name}");
+                return componentsObjects[typeof(T).Name] as T;
+            }
+            public T AddComponent<T>() where T : Node, new()
+            {
+                if (componentsObjects.ContainsKey(typeof(T).Name)) return null;
                 T component = new T();
                 component.Name = typeof(T).Name;
-                node.AddChild(component);
+                AddChild(component);
                 return component;
+            }
+            public static ComponentSystem GetComponentSystemWithTag(string tag)
+            {
+                if (!componentsSystems.ContainsKey(tag)) return null;
+                return componentsSystems[tag];
+            }
+        }
+        public static class NodeExtensions
+        {
+            public static ComponentSystem GetComponentSystem(this Node node)
+            {
+                return node.GetNodeOrNull<ComponentSystem>($"Components");
             }
         }
     }
